e827353ddc429bed366e126d79ae7fdf4ac013ec
“Mehul <kaushikmehul94@gmail.com>
2025-01-22 23:30:24 +0530

    a

--- backend/api/log-ip.js
+++ backend/api/log-ip.js
@@ -1,10 +1,9 @@
 const { MongoClient } = require('mongodb');
 
-// MongoDB connection string (replace with your connection string)
-const uri = "mongodb+srv://mehul:<db_password>@cluster0.pajif.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// Use environment variable for MongoDB URI
+const uri = process.env.MONGODB_URI;
 
 module.exports = async (req, res) => {
-  // Get the user's IP address
   const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   const logEntry = {
     ip,
@@ -16,11 +15,10 @@ module.exports = async (req, res) => {
     const client = await MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
     const db = client.db('ip-logs');
     const collection = db.collection('ip-logs');
-    
+
     await collection.insertOne(logEntry);
     client.close();
 
-    // Respond with success message
     res.status(200).json({ message: 'IP logged successfully' });
   } catch (error) {
     console.error('Error logging IP:', error);
